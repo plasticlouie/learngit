55092c5ea5bb85986c598bca089a6034b3ac4e95
PlasticLouie <xmuxxl@163.com>
2018-06-03 09:30:49 +0800

    master v1

--- model.py
+++ model.py
@@ -1,8 +1,4 @@
 import tensorflow as tf
 import numpy as np
 
-slim = tf.contrib.slim
-
-x = tf.placeholder(tf.float32, shape=(None,32,32,1))
-net = slim.conv2d(x, 32, [3,3])
-net = slim.flatten(net)
+model_name = "master v1"
